7f1dbc108d2cca94e908662d5ef7d63b34fc66e3
t-x-t <contact@thetxt.club>
2020-12-13 18:10:25 +0100

    Added webapi enpoints for getting user, guild and user in guild karma history

--- src/karmaRetriever/webApi.js
+++ src/karmaRetriever/webApi.js
@@ -3,15 +3,21 @@ import discordFetcher from "./discordFetcher.js";
 
 export default async function(req){
   req.path = req.path.replace("/api/v1/", "");
-  switch(req.path){
-    case "toplists/users":
-      return await getTopUsers(req);
-    case "toplists/guilds":
-      return await getTopGuilds(req);
-    case "history/totalkarma":
-      return await getHistoryTotalKarma(req);
-    default:
-      throw new Error("API resource doesnt exist in v1 API");
+
+  if(req.path.startsWith("toplists/users")){
+    return await getTopUsers(req);
+  } else if(req.path.startsWith("toplists/guilds")){
+    return await getTopGuilds(req);
+  } else if(req.path.startsWith("history/totalkarma")){
+    return await getHistoryTotalKarma(req);
+  } else if(req.path.startsWith("history/userkarma/")) {
+    return await getHistoryUserKarma(req);
+  } else if(req.path.startsWith("history/guildkarma/") && req.path.includes("/totalkarma")) {
+    return await getHistoryGuildKarma(req);
+  } else if(req.path.startsWith("history/guildkarma/") && req.path.includes("/userkarma/")) {
+    return await getHistoryUserInGuildKarma(req);
+  } else {
+    throw new Error("API resource doesnt exist in v1 API");
   }
 }
 
@@ -62,6 +68,24 @@ async function getTopGuilds(req){
 
 async function getHistoryTotalKarma(req){
   const hoursInPast = req.query.hoursInPast ? req.query.hoursInPast : 24 * 7;
-  const output = await karmaRetriever.getTotalKarmaHistory(hoursInPast);
-  return output;
+  return await karmaRetriever.getTotalKarmaHistory(hoursInPast);
+}
+
+async function getHistoryUserKarma(req){
+  const hoursInPast = req.query.hoursInPast ? req.query.hoursInPast : 24 * 7;
+  const userId = req.path.split("/")[2];
+  return await karmaRetriever.getUserKarmaHistory(hoursInPast, userId);
+}
+
+async function getHistoryGuildKarma(req){
+  const hoursInPast = req.query.hoursInPast ? req.query.hoursInPast : 24 * 7;
+  const guildId = req.path.split("/")[2];
+  return await karmaRetriever.getGuildKarmaHistory(hoursInPast, guildId);
+}
+
+async function getHistoryUserInGuildKarma(req){
+  const hoursInPast = req.query.hoursInPast ? req.query.hoursInPast : 24 * 7;
+  const userId = req.path.split("/")[4];
+  const guildId = req.path.split("/")[2];
+  return await karmaRetriever.getUserInGuildKarmaHistory(hoursInPast, userId, guildId);
 }
