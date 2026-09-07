5cab0c622658ffe80f40fe88552235f9528d56df
TheTxT <contact@thetxt.io>
2026-09-07 20:00:56 +0200

    get rid of old nuxt website

--- web/pages/docs.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <Header />
-    <iframe src="https://wiki.thetxt.io/en/karmabot" frameborder="0">
-      <a href="https://wiki.thetxt.io/en/karmabot">Couldnt load wiki in iframe, try going there by clicking this</a>
-    </iframe>
-    <Footer />
-  </div>
-</template>
